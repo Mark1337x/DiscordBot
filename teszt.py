8a1100630b18a0b145993d68f5539fbb18f61f9e
Mark1337x <47723568+Mark1337x@users.noreply.github.com>
2019-03-02 12:53:23 +0100

    Update teszt.py

--- teszt.py
+++ teszt.py
@@ -43,7 +43,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content == '!event':
-        await client.send_message(message.channel,**'Hétvégi eventeink a következők:')
+        await client.send_message(message.channel,'**Hétvégi eventeink a következők:')
         await client.send_message(message.channel,'**Okey kártya event __szombat 23:59-ig__**,')
         await client.send_message(message.channel,'**Hatszög kincsesláda event __szombat 23:59-ig__**,')
         await client.send_message(message.channel,'**Holdfény kincsesláda event szombat __23:59-től vasárnap 23:59-ig__**,')
